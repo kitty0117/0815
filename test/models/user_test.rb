de5f6df5ae2389e3950190a174f878f171561f5d
kitty0117 <axx_ra5882@yahoo.co.jp>
2019-08-24 07:50:22 +0000

    Add user microposts

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -49,6 +49,16 @@ class UserTest < ActiveSupport::TestCase
       assert_not @user.valid?, "#{invalid_address.inspect} should be invalid"
     end
   end
+  
+  test "associated microposts should be destroyed" do
+    @user.save
+    @user.microposts.create!(content:"Lorem ipsum")
+    assert_difference 'Micropost.count', -1 do
+      @user.destroy
+    end 
+  end 
+
+  
 
 
 
